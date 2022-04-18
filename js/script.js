c8ae2fec9519ea81565aab69a528cf4c328196f2
mj <103914491+minjikong@users.noreply.github.com>
2022-04-18 13:49:45 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(function(){
     //.submenu_wrap 숨긴다.
-    $(".submenu_wrap").hide();
+    $(".sub").hide();
     //#gnb li에 마우스를 올리면,
-    $("#gnb li").mouseenter(function(){
+    $("#nav li").mouseenter(function(){
         //.submenu_wrap이 슬라이드다운된다.
-        $(".submenu_wrap").stop().slideDown();
+        $(".sub").stop().slideDown();
     });
     //#header(#gnb li+.submenu_wrap)에 마우스를 벗어나면,
     $("#header").mouseleave(function(){
         //.submenu_wrap이 슬라이드업된다.
-      $(".submenu_wrap").stop().slideUp();
+      $(".sub").stop().slideUp();
   });
 });
